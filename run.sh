9b068123fbee37e3e0c3d72f1ffe46e77a8de054
mac <mac@macs-MacBook-Pro.local>
2019-01-26 20:04:46 +0700

    u6

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 RUN firewall-cmd --permanent --add-port=80/tcp
 RUN sudo firewall-cmd --permanent --add-port=443/tcp
 RUN sudo firewall-cmd --reload
-systemctl start httpd
+/usr/sbin/httpd -D FOREGROUND
 exec $@
 
